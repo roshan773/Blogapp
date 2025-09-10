a8f76072535dac8532615b0be390f1473f4a8ea4
Pakhre Roshan <pakhreroshan@gmail.com>
2025-09-10 12:55:42 +0530

    Done with blog app project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
