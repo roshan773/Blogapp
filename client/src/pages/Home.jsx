ef9c888cfce142d7618b3c89cc07f26812de160c
Pakhre Roshan <pakhreroshan@gmail.com>
2025-09-10 12:43:33 +0530

    Done with blog app project

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -38,7 +38,7 @@ const Home = () => {
                 <Card.Body>
                   <Card.Title className="fw-semibold">{blog.title}</Card.Title>
                   <Card.Subtitle className="mb-2 text-muted small">
-                    By {blog.author} &nbsp;•&nbsp; {blog.createdAt?.substring(0, 10)}
+                    By {blog.author} &nbsp;•&nbsp; {blog.createdAt}
                   </Card.Subtitle>
 
                   <Card.Text className="mt-3" style={{ fontSize: '0.95rem', lineHeight: '1.6' }}>
